4bf69ba62a9263ffbc224b4aa87dfbfc03ec09ad
lucas salvatierra <lucassnahuel1759@gmail.com>
2024-01-16 20:56:27 -0300

    asas

--- api/index.js
+++ api/index.js
@@ -1,15 +1,16 @@
-
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+
 
 const saveCountriesController = require("./src/controllers/saveCountriesController.js")
 
 // Syncing all the models at once.
 
-saveCountriesController()
+
 conn.sync({ alter: true }).then(async () => {
+await saveCountriesController()
 console.log("estoy conectado a ",conn.getDatabaseName());
-  server.listen(5432, () => {
+  server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
