9365f8bcadcaf4593d1d72c727bf254fab98bece
lucas salvatierra <lucassnahuel1759@gmail.com>
2024-01-16 20:06:40 -0300

    de-deploy url

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ const saveCountriesController = require("./src/controllers/saveCountriesControll
 saveCountriesController()
 conn.sync({ alter: true }).then(async () => {
 console.log("estoy conectado a ",conn.getDatabaseName());
-  server.listen(3001, () => {
+  server.listen(5432, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
